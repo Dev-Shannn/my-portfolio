659a35186a8f1b81a0abf534a53ec72ad3e05981
Dev-shannn <nishantpandeyy07@gmail.com>
2025-07-04 22:47:22 +0530

    5

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -4,7 +4,7 @@ const AboutMeImage = () => {
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
           src="../../public/images/about-me.jpg"
-          alt="About Me Image"
+          alt=""
           className="h-full w-auto object-cover"
         />
       </div>
